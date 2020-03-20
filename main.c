7f95bc8427a642d107e2d7aa6a16ac16928301b0
João Leal <joao.leal25@hotmail.com>
2020-03-20 18:00:14 +0000

    init

---
+++ main.c
@@ -0,0 +1,12 @@
+/***********************************************
+*          Projeto WordZ 19/20 2S              *
+*                                              *
+*   Joao Leal-89486   &&   Inês Meira-93084    * 
+***********************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main
